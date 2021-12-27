a2b9c1ceff793d53832014cbf8b880b7c0dd38ba
Jason Pekos <Jason.Pekos@gmail.com>
2021-12-27 18:44:19 -0500

    some io stuff, broken still

--- cpp_code/splines.cpp
+++ cpp_code/splines.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <string>
 #include <iostream>
+#include <fstream>
 #include <cmath>
 
 
@@ -16,27 +17,92 @@ int main(int argc, char const *argv[])
     }
 
 
+    std::vector<double> told = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+    std::vector<double> yold = {1,4,21,18,17,22,15,14,10,9,11,8,13,14,17};
+
+    std::vector<double> t = {}; //Empty vectors to push back into
+    std::vector<double> y = {}; 
+
+    std::string line = "";
+    /*
+    Read in input.csv file
+    */
+
+    ifstream data("input.csv");
+    if (!data.is_open()){
+        cout << "Error opening file!";
+        return(-1);
+    }
+    while (std::getline(data, line))
+    {
+        if (line.find_first_of("0123456789") == std::string::npos){
+            continue;
+        }
+
+    }
+    
+
+
+
+
+
+    /*
+    Check to make sure we have time series data. 
+
+    */
+    
+
+
     
     
 
-    std::vector<double> t = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
-    std::vector<double> y = {1,4,21,18,17,22,15,14,10,9,11,8,13,14,17};
 
 
 
-    std::string method(argv[1]);
 
-    Spline model("PolynomialRegression",3, 0);
+    // std::string method(argv[1]);
 
-    model.fit(t,y);
-    double a = model.predict(12);
+    // Spline model("PolynomialRegression",3, 0);
 
-    std::cout << "predicted value:" << a << "\n";
+    // model.fit(t,y);
+    // double a = model.predict(12);
 
-    std::vector<std::vector<double>> qq = model.Coe;
+    // std::cout << "predicted value:" << a << "\n";
 
-    PrintMat(qq);
+    // std::vector<std::vector<double>> qq = model.Coe;
 
+    // PrintMat(qq);
+
+
+
+    /* 
+    file output
+    */
+    std::vector<double> xTemp = {1,4,21,18,17,22,15,14,10,9,11,8,13,14,17}; //Model output.
+    std::vector<double> modelTemp = {1,4,21,18,17,22,15,14,10,9,11,8,13,14,17}; //Model output.
+
+
+    std::ofstream output("output.csv");  //IO check.
+    if (!output.is_open())
+    {
+        std::cout << "Error opening output file";
+        return( -1);
+    } 
+
+    //Output CSV header names.
+    output << "t" << ',';
+    output << "y" << ',';
+    output << '\n';
+
+
+    for (uint64_t i = 0; i < modelTemp.size(); i++)
+    {
+        output << xTemp[i] << ",";
+        output << modelTemp[i] << "," <<  '\n';
+    }
+    
+    output.close();
+    std::cout << "output.csv updated \n"; //
 
     std::cout << "done :D" << "\n";
 
