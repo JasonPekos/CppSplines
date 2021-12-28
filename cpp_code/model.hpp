43d53c7731cc0466f2955aa1eb2e16febd6faab4
Jason Pekos <Jason.Pekos@gmail.com>
2021-12-28 00:09:35 -0500

    clean

--- cpp_code/model.hpp
+++ cpp_code/model.hpp
@@ -23,23 +23,33 @@ private:
     uint64_t Knots = 3; //Data about model (number of knots)
     uint64_t Power = 3; //Data about model (power of knots)
     std::string Method = "PowerBasis"; //Data about model (Method) [PowerBasis or BSpline or PolynomialRegression] 
-     //Holds the actual knot positions. 
 
 public:
 
+//Holds the coefficients. Initialized at zero.
 std::vector<std::vector<double>> Coe{1,std::vector<double>(1,0)};
+
+//Holds the actual knot positions. Filled when fitting the model.
 std::vector<double> kTemp = {};
 
 void fit(std::vector<double> t, std::vector<double> y){
+    /**
+     * @brief Fit the model on data (t,y).
+     * 
+     */
 
-        //If asked for PowerBasis
+        //If asked for PowerBasis, continue by fitting with the following method:
         if (Method == "PowerBasis")
         {
-            kTemp = {};
+            //Empty out the knot positions vector, so we can push into the back safely. 
+            kTemp = {}; 
             if (Knots > 0)
             {
+                /*
+                Place 'Knots' evenly spaced knots over the range of time values. 
+                */
                 double range = t.back() - t.front();
-                double kEvery  = range / Knots;
+                double kEvery  = range / (Knots + 1);
                 double kCurrent = kEvery;
                 while (kCurrent < t.back())
                 {
@@ -50,16 +60,36 @@ void fit(std::vector<double> t, std::vector<double> y){
             }
             else
             {
+                /*
+                If this is somehow being called without CL args (e.g. during code reuse), call an error if knots <= 0. 
+                */
                 std::cout << "error: at least one knot required" << "\n";
                 exit(-1);
             }
 
+            //Create the design matrix for this program using the LinAlg function. 
             std::vector<std::vector<double>> DesignMatrix = DesignPowerBasis(t, Power, kTemp);
 
-
+            //Solve as a linear system. 
             std::vector<std::vector<double>> XTX = MatMul(Transpose(DesignMatrix), DesignMatrix);
 
+            //Return Coefficients.
             Coe = SolveSystem(XTX, MatVecMul(Transpose(DesignMatrix), y));
+        }
+
+
+
+        //If asked for polynomial regression. 
+        if (Method == "PolynomialRegression")
+        {
+            //Warning if you asked for knots on a method that doesn't support them.
+            if (Knots > 0) 
+            {
+                std::cout << "Warning: Knots parameter only applies to Bspline and PowerBasis methods.";
+            }
+            //Create design matrix and solve as a linear system, e.g. X'X \ X'y
+            std::vector<std::vector<double>> DesignMatrix = Design(t,Power);
+            Coe = SolveSystem(MatMul(Transpose(DesignMatrix), DesignMatrix), MatVecMul(Transpose(DesignMatrix), y));
         }
 
         //If asked for Basis Spline
@@ -67,24 +97,23 @@ void fit(std::vector<double> t, std::vector<double> y){
         {
            std::cout << "Not Implemented Yet" << "\n";
         }
-
-        //If asked for polynomial regression. 
-        if (Method == "PolynomialRegression")
-        {
-            if (Knots > 0)
-            {
-                std::cout << "Warning: Knots parameter only applies to Bspline and PowerBasis methods.";
-            }
-            std::vector<std::vector<double>> DesignMatrix = Design(t,Power);
-            Coe = SolveSystem(MatMul(Transpose(DesignMatrix), DesignMatrix), MatVecMul(Transpose(DesignMatrix), y));
-        }
     }
 
     double predict(double t){
+        /**
+         * @brief Predict output given model + input
+         * 
+         * @param t Value we need to predict at, e.g. return Model(t)
+         * 
+         * -> sum(coe_n*basisFuction_n (t)) for all n. 
+         * 
+         */
+
+        //Define value we sum into. 
         double val = 0;
         if (Method == "PolynomialRegression")
         {
-            
+            //Again, sum(coe_n*basisFuction_n (t)) for all n.
             for (uint64_t i = 0; i < Coe.size(); i++)
             {
                 val += Coe[i][0]*pow(t, i);
@@ -92,14 +121,13 @@ void fit(std::vector<double> t, std::vector<double> y){
         }
         if (Method == "PowerBasis")
         {
-            //Before the power basis functions. 
-
+            //Before the explicitly power basis functions. 
             val = 0;
             for (uint64_t i = 0; i < Power + 1; i++)
             {
                 val += Coe[i][0]*pow(t, i);
             }
-            
+            //Power basis functions, e.g. coe * +_(t - knot)^max_power. 
             for (uint64_t i = Power + 1; i < Coe.size(); i++)
             {
                 val += Coe[i][0]*pow(pm(t - kTemp[i - (Power + 1 )]), Power);
@@ -109,18 +137,40 @@ void fit(std::vector<double> t, std::vector<double> y){
     }
 
     Spline(std::string method, uint64_t power, uint64_t knots){
-        /* constructor --- add error checking 
+        /**
+         * @brief constructor
+         * 
+         * @param knots int64_t, positive value.
+         * @param method string, matches "PowerBasis, BSpline", "PolynomialRegression".
+         * @param power int64_t, nth order splines, positive or zero.
+         */
 
-        knots -> int64_t, positive value
-        method -> string, matches "PowerBasis, BSpline"
-        power -> int64_t, nth order splines, positive or zero, 
+        //Strings to check against in constructor. 
+        std::string BSP = "BSpline";
+        std::string PBS = "PowerBasis";
+        std::string PR  = "PolynomialRegression";
 
-
-        */
+        //Assign values. 
         Knots = knots;
         Power = power;
         Method = method;
-        
+
+        //Simple IO check in the case of explicit construction outside of CL arguments --- makes the class safer. 
+        if (knots < 0)
+        {
+            std::cout << "Error: can't construct with a negative number of knots!" << "\n";
+            throw std::invalid_argument( "received negative value" );
+        }
+        if (power < 1)
+        {
+            std::cout << "Error: can't construct with a power less than one!" << "\n";
+            throw std::invalid_argument( "received illegitimate value" );
+        }
+        if (method != BSP && method != PBS && method != PR )
+        {
+            std::cout << "Error: submit a valid method!" << "\n";
+            throw std::invalid_argument( "received illegitimate method" );
+        }
     }
 };
 
--- cpp_code/splines.cpp
+++ cpp_code/splines.cpp
@@ -12,56 +12,74 @@
 int main(int argc, char const *argv[])
 {
 
+    //Quick IO check, moved into a header file. 
     if(InputCheck(argc, argv) == 0){
+        std::cout << "Failed Input Output Check" << "\n";
         return(-1);
     }
 
+    //Method CL argument into string. 
     std::string method(argv[1]);
 
+    //In the case of no knot points being provided, simply use the un-augmented polynomial regression function. 
     if (InputCheck(argc, argv) == 2)
     {
         method = "PolynomialRegression"; //If we don't have any knot points, just use PNR as the default. 
     }
+
+    //Bring over knots and power CL arguments.
     std::string Arg2(argv[2]);
     std::string Arg3(argv[3]);
 
+    //Case to fixed width values. 
     uint64_t power = (uint64_t)stoi(Arg2);
     uint64_t knots = (uint64_t)stoi(Arg3);
     
 
-    std::vector<double> t = {}; //Empty vectors to push back into
+    //Empty vectors to push .CSV input back into
+    std::vector<double> t = {}; 
     std::vector<double> y = {};
 
-    std::string xVal; //temp variables to store IOStream values.
+    //temp variables to store IOStream values.  
+    std::string xVal; 
     std::string yVal;
 
     /*
     Read in input.csv file
     */
 
+    //Inputstream to the .csv we care about. 
     ifstream data("input.csv");
+
+    //Check to make sure file opened correctly. 
     if (!data.is_open()){
         cout << "Error opening file!";
         return(-1);
     }
+
+    //Read in data until we hit the end of the file. 
     while (data.peek()!= EOF)
     {
 
-        std::getline(data, xVal, ',');
+        //Push data into xVal, yVal with ',' and newline delimiters.
+        std::getline(data, xVal, ','); 
         std::getline(data, yVal, '\n');
 
 
+        //Check for legitimate input. Must be float. Can be negative and spline still well defined. 
         if (xVal.find_first_not_of("-1234567890.")!= std::string::npos)
         {
             std::cout << "Error with .csv input file";
             return(-1);
         }
+
         if (yVal.find_first_not_of("-1234567890.")!= std::string::npos)
         {
             std::cout << "Error with .csv input file";
             return(-1);
         }
 
+        //Push out value for this loop into the data vector. 
         t.push_back(std::stod(xVal));
         y.push_back(std::stod(yVal));
     }
@@ -80,23 +98,19 @@ int main(int argc, char const *argv[])
     Spline model(method, power, knots);
     model.fit(t,y);
 
-
-    std::vector<std::vector<double>> qq = model.Coe;
-
-
-    /* 
-    file output
-    */
+    //Temp vectors to hold predictions. 
     std::vector<double> xTemp = t; //Model output.
     std::vector<double> modelTemp = {}; //Model output.
 
+    //Predict values at each timepoint in our initial dataset. 
     for (uint64_t i = 0; i < t.size(); i++)
     {
         modelTemp.push_back(model.predict(t[i]));
     }
     
-
-
+    /* 
+    file output
+    */
     std::ofstream output("output.csv");  //IO check.
     if (!output.is_open())
     {
@@ -109,7 +123,7 @@ int main(int argc, char const *argv[])
     output << "y";
     output << '\n';
 
-
+    //Populate .CSV.
     for (uint64_t i = 0; i < modelTemp.size(); i++)
     {
         output << xTemp[i] << ",";
